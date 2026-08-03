cc3fe8179a7ec21d03d9d9d22b33d7db634f9658
Diana Carrillo <itsdianet@gmail.com>
2026-08-03 01:24:58 -0700

    Actualizar elementos.js

--- elementos.js
+++ elementos.js
@@ -4,28 +4,32 @@ const elementos = [
         simbolo: "H",
         nombre: "Hidrógeno",
         columna: 1,
-        fila: 1
+        fila: 1,
+        tipo: "no-metales"
     },
     {
         numero: 2,
         simbolo: "He",
         nombre: "Helio",
         columna: 18,
-        fila: 1
+        fila: 1,
+        tipo: "no-metales"
     },
     {
         numero: 3,
         simbolo: "Li",
         nombre: "Litio",
         columna: 1,
-        fila: 2
+        fila: 2,
+        tipo: "metales"
     },
     {
         numero: 4,
         simbolo: "Be",
         nombre: "Berilio",
         columna: 2,
-        fila: 2
+        fila: 2,
+        tipo: "metales"
     }
 ];
 
@@ -36,7 +40,7 @@ const tabla = document.getElementById("tabla");
 elementos.forEach(elemento => {
 
     tabla.innerHTML += `
-        <div class="elemento" 
+        <div class="elemento ${elemento.tipo}" 
         style="grid-column:${elemento.columna}; grid-row:${elemento.fila};">
             
             <h2>${elemento.simbolo}</h2>
