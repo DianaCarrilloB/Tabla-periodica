a18688849e7a85c750924bac8adf52a5c423d572
Diana Carrillo <itsdianet@gmail.com>
2026-08-03 01:13:01 -0700

    Actualizar elementos.js

--- elementos.js
+++ elementos.js
@@ -2,23 +2,48 @@ const elementos = [
     {
         numero: 1,
         simbolo: "H",
-        nombre: "Hidrógeno"
+        nombre: "Hidrógeno",
+        columna: 1,
+        fila: 1
     },
     {
         numero: 2,
         simbolo: "He",
-        nombre: "Helio"
+        nombre: "Helio",
+        columna: 18,
+        fila: 1
+    },
+    {
+        numero: 3,
+        simbolo: "Li",
+        nombre: "Litio",
+        columna: 1,
+        fila: 2
+    },
+    {
+        numero: 4,
+        simbolo: "Be",
+        nombre: "Berilio",
+        columna: 2,
+        fila: 2
     }
 ];
 
+
 const tabla = document.getElementById("tabla");
 
+
 elementos.forEach(elemento => {
+
     tabla.innerHTML += `
-        <div class="elemento">
+        <div class="elemento" 
+        style="grid-column:${elemento.columna}; grid-row:${elemento.fila};">
+            
             <h2>${elemento.simbolo}</h2>
             <p>${elemento.nombre}</p>
             <p>${elemento.numero}</p>
+
         </div>
     `;
+
 });
