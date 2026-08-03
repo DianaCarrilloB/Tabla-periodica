6f3ba0c2f984ac31c8581fbe97df002ec372dbb3
Diana Carrillo <itsdianet@gmail.com>
2026-08-03 15:54:30 -0700

    Actualizar elementos.js

--- elementos.js
+++ elementos.js
@@ -78,6 +78,70 @@ const elementos = [
         columna: 18,
         fila: 2,
         tipo: "gases-nobles"
+    },
+    {
+        numero: 11,
+        simbolo: "Na",
+        nombre: "Sodio",
+        columna: 1,
+        fila: 3,
+        tipo: "alcalinos"
+    },
+    {
+        numero: 12,
+        simbolo: "Mg",
+        nombre: "Magnesio",
+        columna: 2,
+        fila: 3,
+        tipo: "alcalinoterreos"
+    },
+    {
+        numero: 13,
+        simbolo: "Al",
+        nombre: "Aluminio",
+        columna: 13,
+        fila: 3,
+        tipo: "postransicion"
+    },
+    {
+        numero: 14,
+        simbolo: "Si",
+        nombre: "Silicio",
+        columna: 14,
+        fila: 3,
+        tipo: "metaloides"
+    },
+    {
+        numero: 15,
+        simbolo: "P",
+        nombre: "Fósforo",
+        columna: 15,
+        fila: 3,
+        tipo: "no-metales"
+    },
+    {
+        numero: 16,
+        simbolo: "S",
+        nombre: "Azufre",
+        columna: 16,
+        fila: 3,
+        tipo: "no-metales"
+    },
+    {
+        numero: 17,
+        simbolo: "Cl",
+        nombre: "Cloro",
+        columna: 17,
+        fila: 3,
+        tipo: "halogenos"
+    },
+    {
+        numero: 18,
+        simbolo: "Ar",
+        nombre: "Argón",
+        columna: 18,
+        fila: 3,
+        tipo: "gases-nobles"
     }
     ];
 
