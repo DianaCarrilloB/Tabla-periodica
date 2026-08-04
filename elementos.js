747b2c923bafefc4eab0ef0f728963cbb08d8c7d
Diana Carrillo <itsdianet@gmail.com>
2026-08-04 00:04:41 -0700

    Actualizar elementos.js

--- elementos.js
+++ elementos.js
@@ -286,6 +286,150 @@ const elementos = [
         columna: 18,
         fila: 4,
         color: "grupo18"
+    },
+    {
+        numero: 37,
+        simbolo: "Rb",
+        nombre: "Rubidio",
+        columna: 1,
+        fila: 5,
+        color: "grupo1"
+    },
+    {
+        numero: 38,
+        simbolo: "Sr",
+        nombre: "Estroncio",
+        columna: 2,
+        fila: 5,
+        color: "grupo2"
+    },
+    {
+        numero: 39,
+        simbolo: "Y",
+        nombre: "Itrio",
+        columna: 3,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 40,
+        simbolo: "Zr",
+        nombre: "Circonio",
+        columna: 4,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 41,
+        simbolo: "Nb",
+        nombre: "Niobio",
+        columna: 5,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 42,
+        simbolo: "Mo",
+        nombre: "Molibdeno",
+        columna: 6,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 43,
+        simbolo: "Tc",
+        nombre: "Tecnecio",
+        columna: 7,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 44,
+        simbolo: "Ru",
+        nombre: "Rutenio",
+        columna: 8,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 45,
+        simbolo: "Rh",
+        nombre: "Rodio",
+        columna: 9,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 46,
+        simbolo: "Pd",
+        nombre: "Paladio",
+        columna: 10,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 47,
+        simbolo: "Ag",
+        nombre: "Plata",
+        columna: 11,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 48,
+        simbolo: "Cd",
+        nombre: "Cadmio",
+        columna: 12,
+        fila: 5,
+        color: "transicion"
+    },
+    {
+        numero: 49,
+        simbolo: "In",
+        nombre: "Indio",
+        columna: 13,
+        fila: 5,
+        color: "grupo13"
+    },
+    {
+        numero: 50,
+        simbolo: "Sn",
+        nombre: "Estaño",
+        columna: 14,
+        fila: 5,
+        color: "grupo14"
+    },
+    {
+        numero: 51,
+        simbolo: "Sb",
+        nombre: "Antimonio",
+        columna: 15,
+        fila: 5,
+        color: "grupo15"
+    },
+    {
+        numero: 52,
+        simbolo: "Te",
+        nombre: "Telurio",
+        columna: 16,
+        fila: 5,
+        color: "grupo16"
+    },
+    {
+        numero: 53,
+        simbolo: "I",
+        nombre: "Yodo",
+        columna: 17,
+        fila: 5,
+        color: "grupo17"
+    },
+    {
+        numero: 54,
+        simbolo: "Xe",
+        nombre: "Xenón",
+        columna: 18,
+        fila: 5,
+        color: "grupo18"
     }
     ];
 
